b962033c9e03e1953f7a949353099392a1d05128
autumnesponda <autumnesponda@knights.ucf.edu>
2022-09-17 12:46:03 -0400

    completed basic web interface to view scraped data; added example .csv files

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import {ActiveElement, Chart, ChartConfiguration, ChartEvent} from "chart.js";
+import {PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,108 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  public columnHeaders: string[] = [];
+  public dropdownOptions: string[] = [];
+
+  public selectedXAxis = '';
+  public selectedYAxis = '';
+
+  public scatterChartDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
+    {
+      data: [],
+      label: '',
+      pointRadius: 10,
+    },
+  ];
+
+  public scatterChartOptions: ChartConfiguration<'scatter'>['options'] = {
+    responsive: true,
+    onClick(event: ChartEvent, elements: ActiveElement[], chart: Chart) {
+      if (elements.length > 0) {
+        const element = elements[0];
+        const rawElement = chart.data.datasets[0].data[element.index] as any;
+        const url = rawElement['Url'];
+        if (url)
+          window.open(url, '_blank')?.focus();
+      }
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context: any) => {
+            const xLabel = this.selectedXAxis;
+            const yLabel = this.selectedYAxis;
+            const title = context.raw['Title'];
+            return `${title}\n${xLabel} ${context.parsed.x}; ${yLabel}: ${context.parsed.y}`;
+          }
+        }
+      }
+    }
+  };
+
+  constructor(private ngxCsvParser: NgxCsvParser, private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() { this.primengConfig.ripple = true; }
+
+  updateSelectedXAxis($event: any): void {
+    this.selectedXAxis = $event.value;
+    this.setScatterChartDataFromSelectedHeaders();
+  }
+  updateSelectedYAxis($event: any): void {
+    this.selectedYAxis = $event.value;
+    this.setScatterChartDataFromSelectedHeaders();
+  }
+
+  // read in .csv
+  onFileSelected($event: Event): void {
+    const file = ($event.target as HTMLInputElement).files?.[0];
+    if (!file) { return; }
+
+    this.ngxCsvParser.parse(file, { header: true, delimiter: ',' })
+      .pipe().subscribe((result) => {
+        if (result instanceof NgxCSVParserError) {
+          alert('Error parsing .csv: ' + result.message);
+        } else {
+          if (result.length > 0) {
+            if (this.validateHeaders(result[0])) {
+              this.columnHeaders = Object.getOwnPropertyNames(result[0])
+              this.columnHeaders.forEach((h: any) => {
+                // only add headers that have numeric values to the dropdowns
+                if (isFinite(result[0][h])) {
+                  this.dropdownOptions.push(h);
+                }
+              });
+
+              // this jank is necessary to get the dropdowns to update
+              this.selectedXAxis = this.dropdownOptions[this.dropdownOptions.indexOf('Current # Watchers')];
+              this.selectedYAxis = this.dropdownOptions[this.dropdownOptions.indexOf('Current Price')];
+
+              this.scatterChartDatasets[0].data = result;
+              this.setScatterChartDataFromSelectedHeaders();
+            } else {
+              alert('Error parsing .csv: Invalid headers');
+            }
+          }
+          else {
+            alert('Error parsing .csv: No data found');
+          }
+        }
+      }, (error: NgxCSVParserError) => {
+          alert('Error parsing .csv: ' + error.message);
+      });
+  }
+
+  private setScatterChartDataFromSelectedHeaders(): void {
+    this.scatterChartDatasets[0].data.forEach((element: any) => {
+      element.x = element[this.selectedXAxis];
+      element.y = element[this.selectedYAxis];
+    });
+
+    // force change detection to update chart
+    this.scatterChartDatasets = [...this.scatterChartDatasets];
+  }
+
+  private validateHeaders(arrayElement: any) {
+    return arrayElement.hasOwnProperty('Current # Watchers') && arrayElement.hasOwnProperty('Current Price') && arrayElement.hasOwnProperty('Title');
+  }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgChartsModule,
+    DropdownModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
